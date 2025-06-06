80f5397cc2b2c07a6dd483c281dc32458fd0729c
nathan-lowry <nathan.lowry@student.csd509j.net>
2025-06-06 09:39:02 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ let inHub = true
 // classes
 
 // functions
-function loadHub () {
+function loadHub(): void {
     tiles.setCurrentTilemap(tilemap`hub`)
     let playerHolder = tiles.getTilesByType(assets.tile`playerPH`)
     for (let i = 0; i < playerHolder.length; i++) {
@@ -24,6 +24,10 @@ function loadHub () {
     tiles.placeOnTile(signpostSprite, tiles.getTileLocation(7, 4))
     let shopSprite = sprites.create(assets.image`shop`, SpriteKind.Shop)
     tiles.placeOnTile(shopSprite, tiles.getTileLocation(1, 2))
+}
+
+function loadLevel(levelNumber: number): void {
+    
 }
 // event handlers
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Signpost, function(sprite: Sprite, otherSprite: Sprite) {
